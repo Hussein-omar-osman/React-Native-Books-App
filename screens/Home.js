fb4c284657228b1a97cc5be14357854661a7ce59
Hussein Omar <husseinomar6190@gmail.com>
2022-09-06 19:17:21 +0300

    creating buttonsSection & lineDivider component

--- screens/Home.js
+++ screens/Home.js
@@ -8,6 +8,7 @@ import {
   ScrollView,
   FlatList,
 } from 'react-native';
+import ButtonsSection from '../components/ButtonsSection';
 import Header from '../components/Header';
 
 import { COLORS, FONTS, SIZES, icons, images } from '../constants';
@@ -120,128 +121,6 @@ const Home = ({ navigation }) => {
   const [myBooks, setMyBooks] = React.useState(myBooksData);
   const [categories, setCategories] = React.useState(categoriesData);
   const [selectedCategory, setSelectedCategory] = React.useState(1);
-
-  function renderButtonSection() {
-    return (
-      <View
-        style={{ flex: 1, justifyContent: 'center', padding: SIZES.padding }}
-      >
-        <View
-          style={{
-            flexDirection: 'row',
-            height: 70,
-            backgroundColor: COLORS.secondary,
-            borderRadius: SIZES.radius,
-          }}
-        >
-          {/* Claim */}
-          <TouchableOpacity
-            style={{ flex: 1 }}
-            onPress={() => console.log('Claim')}
-          >
-            <View
-              style={{
-                flex: 1,
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <Image
-                source={icons.claim_icon}
-                resizeMode='contain'
-                style={{
-                  width: 30,
-                  height: 30,
-                }}
-              />
-              <Text
-                style={{
-                  marginLeft: SIZES.base,
-                  ...FONTS.body3,
-                  color: COLORS.white,
-                }}
-              >
-                Claim
-              </Text>
-            </View>
-          </TouchableOpacity>
-
-          {/* Divider */}
-          <LineDivider />
-
-          {/* Get Point */}
-          <TouchableOpacity
-            style={{ flex: 1 }}
-            onPress={() => console.log('Get Point')}
-          >
-            <View
-              style={{
-                flex: 1,
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <Image
-                source={icons.point_icon}
-                resizeMode='contain'
-                style={{
-                  width: 30,
-                  height: 30,
-                }}
-              />
-              <Text
-                style={{
-                  marginLeft: SIZES.base,
-                  ...FONTS.body3,
-                  color: COLORS.white,
-                }}
-              >
-                Get Point
-              </Text>
-            </View>
-          </TouchableOpacity>
-
-          {/* Divider */}
-          <LineDivider />
-
-          {/* My Card */}
-          <TouchableOpacity
-            style={{ flex: 1 }}
-            onPress={() => console.log('My Card')}
-          >
-            <View
-              style={{
-                flex: 1,
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <Image
-                source={icons.card_icon}
-                resizeMode='contain'
-                style={{
-                  width: 30,
-                  height: 30,
-                }}
-              />
-              <Text
-                style={{
-                  marginLeft: SIZES.base,
-                  ...FONTS.body3,
-                  color: COLORS.white,
-                }}
-              >
-                My Card
-              </Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  }
 
   function renderMyBookSection(myBooks) {
     const renderItem = ({ item, index }) => {
@@ -567,7 +446,7 @@ const Home = ({ navigation }) => {
       <View style={{ height: 200, marginTop: 20 }}>
         {/* {Header(profile)} */}
         <Header profile={profile} />
-        {renderButtonSection()}
+        <ButtonsSection />
       </View>
 
       {/* Body Section */}
@@ -587,81 +466,5 @@ const Home = ({ navigation }) => {
     </SafeAreaView>
   );
 };
-
-// function Header({ profile }) {
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         flexDirection: 'row',
-//         paddingHorizontal: SIZES.padding,
-//         alignItems: 'center',
-//       }}
-//     >
-//       {/* Greetings */}
-//       <View style={{ flex: 1 }}>
-//         <View style={{ marginRight: SIZES.padding }}>
-//           <Text style={{ ...FONTS.h3, color: COLORS.white }}>Good Morning</Text>
-//           <Text style={{ ...FONTS.h2, color: COLORS.white }}>
-//             {profile.name}
-//           </Text>
-//         </View>
-//       </View>
-
-//       {/* Points */}
-//       <TouchableOpacity
-//         style={{
-//           backgroundColor: COLORS.primary,
-//           height: 40,
-//           paddingLeft: 3,
-//           paddingRight: SIZES.radius,
-//           borderRadius: 20,
-//         }}
-//         onPress={() => {
-//           console.log('Point');
-//         }}
-//       >
-//         <View
-//           style={{
-//             flex: 1,
-//             flexDirection: 'row',
-//             justifyContent: 'center',
-//             alignItems: 'center',
-//           }}
-//         >
-//           <View
-//             style={{
-//               width: 30,
-//               height: 30,
-//               alignItems: 'center',
-//               justifyContent: 'center',
-//               borderRadius: 25,
-//               backgroundColor: 'rgba(0,0,0,0.5)',
-//             }}
-//           >
-//             <Image
-//               source={icons.plus_icon}
-//               resizeMode='contain'
-//               style={{
-//                 width: 20,
-//                 height: 20,
-//               }}
-//             />
-//           </View>
-
-//           <Text
-//             style={{
-//               marginLeft: SIZES.base,
-//               color: COLORS.white,
-//               ...FONTS.body3,
-//             }}
-//           >
-//             {profile.point} point
-//           </Text>
-//         </View>
-//       </TouchableOpacity>
-//     </View>
-//   );
-// }
 
 export default Home;
