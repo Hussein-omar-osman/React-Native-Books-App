bf1b849560454708984edc03fefb5a0329bc5bf6
Hussein Omar <husseinomar6190@gmail.com>
2022-11-19 22:27:09 +0300

    add: displayed fetched data

--- screens/Search.js
+++ screens/Search.js
@@ -1,10 +1,21 @@
-import { StyleSheet, Text, View, SafeAreaView, TextInput } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import React, { useEffect, useState } from 'react';
-import { COLORS } from '../constants';
+import { COLORS, FONTS, SIZES, icons, images } from '../constants';
 import { AntDesign, FontAwesome } from '@expo/vector-icons';
 import getData from '../constants/getData';
+import { useNavigation } from '@react-navigation/native';
 
 const Search = () => {
+  const navigation = useNavigation();
   const [booksData, setBooksData] = useState({});
   const getData = async (search) => {
     try {
@@ -19,10 +30,168 @@ const Search = () => {
   };
   useEffect(() => {
     getData('React')
-      .then((data) => setBooksData(data))
+      .then((data) => {
+        setBooksData(data);
+        console.log('something came back');
+      })
       .catch((e) => console.log(e));
   }, []);
-  console.log(booksData);
+  // console.log(booksData);
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={{ marginVertical: 15 }}>
+        <TouchableOpacity
+          style={{ flex: 1, flexDirection: 'row' }}
+          // onPress={() =>
+          //   navigation.navigate('BookDetail', {
+          //     book: item,
+          //   })
+          // }
+        >
+          {/* Book Cover */}
+          <Image
+            source={{
+              uri: item.volumeInfo.imageLinks.thumbnail,
+            }}
+            resizeMode='cover'
+            style={{ width: 100, height: 150, borderRadius: 10 }}
+          />
+
+          <View style={{ flex: 1, marginLeft: SIZES.radius }}>
+            {/* Book name and author */}
+            <View>
+              <Text
+                style={{
+                  paddingRight: SIZES.padding,
+                  ...FONTS.h2,
+                  color: COLORS.white,
+                }}
+              >
+                {item.volumeInfo.title}
+              </Text>
+              <Text style={{ ...FONTS.h3, color: COLORS.lightGray }}>
+                {item.volumeInfo.authors[0]}
+              </Text>
+            </View>
+
+            {/* Book Info */}
+            <View style={{ flexDirection: 'row', marginTop: SIZES.radius }}>
+              <Image
+                source={icons.page_filled_icon}
+                resizeMode='contain'
+                style={{
+                  width: 20,
+                  height: 20,
+                  tintColor: COLORS.lightGray,
+                }}
+              />
+              <Text
+                style={{
+                  ...FONTS.body4,
+                  color: COLORS.lightGray,
+                  paddingHorizontal: SIZES.radius,
+                }}
+              >
+                {item.volumeInfo.pageCount}
+              </Text>
+
+              <Image
+                source={icons.read_icon}
+                resizeMode='contain'
+                style={{
+                  width: 20,
+                  height: 20,
+                  tintColor: COLORS.lightGray,
+                }}
+              />
+              <Text
+                style={{
+                  ...FONTS.body4,
+                  color: COLORS.lightGray,
+                  paddingHorizontal: SIZES.radius,
+                }}
+              >
+                {item.volumeInfo.publishedDate}
+              </Text>
+            </View>
+
+            {/* Genre */}
+            {/* <View style={{ flexDirection: 'row', marginTop: SIZES.base }}>
+              {item.genre.includes('Adventure') && (
+                <View
+                  style={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: SIZES.base,
+                    marginRight: SIZES.base,
+                    backgroundColor: COLORS.darkGreen,
+                    height: 40,
+                    borderRadius: SIZES.radius,
+                  }}
+                >
+                  <Text style={{ ...FONTS.body3, color: COLORS.lightGreen }}>
+                    Adventure
+                  </Text>
+                </View>
+              )}
+              {item.genre.includes('Romance') && (
+                <View
+                  style={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: SIZES.base,
+                    marginRight: SIZES.base,
+                    backgroundColor: COLORS.darkRed,
+                    height: 40,
+                    borderRadius: SIZES.radius,
+                  }}
+                >
+                  <Text style={{ ...FONTS.body3, color: COLORS.lightRed }}>
+                    Romance
+                  </Text>
+                </View>
+              )}
+              {item.genre.includes('Drama') && (
+                <View
+                  style={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: SIZES.base,
+                    marginRight: SIZES.base,
+                    backgroundColor: COLORS.darkBlue,
+                    height: 40,
+                    borderRadius: SIZES.radius,
+                  }}
+                >
+                  <Text style={{ ...FONTS.body3, color: COLORS.lightBlue }}>
+                    Drama
+                  </Text>
+                </View>
+              )}
+            </View> */}
+          </View>
+        </TouchableOpacity>
+
+        {/* Bookmark Button */}
+        <TouchableOpacity
+          style={{ position: 'absolute', top: 5, right: 15 }}
+          onPress={() => console.log('Bookmark')}
+        >
+          <Image
+            source={icons.bookmark_icon}
+            resizeMode='contain'
+            style={{
+              width: 25,
+              height: 25,
+              tintColor: COLORS.lightGray,
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.black }}>
       <View
@@ -31,6 +200,7 @@ const Search = () => {
           padding: 15,
           justifyContent: 'center',
           alignItems: 'center',
+          marginTop: 7,
         }}
       >
         <AntDesign
@@ -38,6 +208,7 @@ const Search = () => {
           name='leftcircle'
           size={27}
           color={COLORS.primary}
+          onPress={() => navigation.navigate('Home')}
         />
         <TextInput
           style={{
@@ -56,6 +227,21 @@ const Search = () => {
           // color={COLORS.primary}
           color='#EFF5F5'
         />
+      </View>
+      <View style={{ marginTop: 5 }}>
+        <View
+          style={{
+            marginTop: SIZES.radius,
+            paddingLeft: 10,
+          }}
+        >
+          <FlatList
+            data={booksData.items}
+            keyExtractor={(item) => item.id}
+            renderItem={renderItem}
+            showsVerticalScrollIndicator={false}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
